03832ff01bb05401e17a621c119fb4b967174118
aldiDR <aldiarema91@gmail.com>
2021-01-14 16:29:28 +0700

    Add starter React JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './feature/'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+	
+	componentDidMount(){
+		if (!localStorage.getItem('todoList')) {
+			let todo_list = [
+				{
+					id: 1,
+					title: "Make a meal",
+					description: "lorem ipsum",
+					status: 0,
+					createdAt: "2019-11-15 18:00"
+				},
+				{
+					id: 2,
+					title: "Dinner with family",
+					description: "lorem ipsum",
+					status: 0,
+					createdAt: "2019-11-16 18:00"
+				},
+				{
+					id: 3,
+					title: "Watch scary movie",
+					description: "lorem ipsum",
+					status: 0,
+					createdAt: "2019-11-15 13:00"
+				},
+				{
+					id: 4,
+					title: "Learn something new",
+					description: "lorem ipsum",
+					status: 1,
+					createdAt: "2019-11-15 08:00"
+				},
+				{
+					id: 5,
+					title: "Make a phone call to mom",
+					description: "lorem ipsum",
+					status: 1,
+					createdAt: "2019-11-15 04:00"
+				}
+			]
+
+
+			localStorage.setItem('todoList', JSON.stringify(todo_list));
+		}
+		
+	}
+
+	render() {
+		
+		return (
+			<TodoList/>
+			);
+		}
+	}
 
 export default App;
